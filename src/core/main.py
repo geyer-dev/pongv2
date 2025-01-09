2d2fa1336553c8e4443bd70fc8158bd9a517c747
Geyer Artúr <geyerartur.dev@proton.me>
2025-01-09 22:13:15 +0100

    Added Button Drop Shadow

--- src/core/main.py
+++ src/core/main.py
@@ -1,4 +1,4 @@
-import time 
+import time
 import pygame
 import os
 import sys
@@ -15,23 +15,67 @@ clock = pygame.time.Clock()
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 font = pygame.font.Font("../../assets/fonts/ARCADECLASSIC.TTF", 55)
-button_text = font.render("play", True, (255,255,255))
-button_rect = button_text.get_rect(center=(WINDOW_WIDTH / 2, (WINDOW_HEIGHT / 100) * 45 ))
+start_text = font.render("play", True, (255, 255, 255))
+options_text = font.render("options", True, (255, 255, 255))
+exit_text = font.render("exit", True, (255, 255, 255))
+
+# Set up button rectangles
+start_rect = start_text.get_rect(center=(WINDOW_WIDTH / 2, (WINDOW_HEIGHT / 100) * 45))
+options_rect = options_text.get_rect(center=(WINDOW_WIDTH / 2, (WINDOW_HEIGHT / 100) * 60))
+exit_rect = exit_text.get_rect(center=(WINDOW_WIDTH / 2, (WINDOW_HEIGHT / 100) * 75))
 
 while True:
     screen.fill("black")
+
+    # Default button rendering
+    start_text = font.render("play", True, (255, 255, 255))
+    options_text = font.render("options", True, (255, 255, 255))
+    exit_text = font.render("exit", True, (255, 255, 255))
+
+    # Check if buttons are hovered
+    mouse_pos = pygame.mouse.get_pos()
+    if start_rect.collidepoint(mouse_pos):
+        shadow_text = font.render("play", True, (0, 155, 155))
+        shadow_rect = start_rect.copy()
+        shadow_rect.x -= 1
+        shadow_rect.y += 4
+        screen.blit(shadow_text, shadow_rect)
+        start_text = font.render("play", True, (0, 255, 0))
+
+    if options_rect.collidepoint(mouse_pos):
+        shadow_text = font.render("options", True, (0, 0, 255))
+        shadow_rect = options_rect.copy()
+        shadow_rect.x += 3
+        shadow_rect.y -= 3
+        screen.blit(shadow_text, shadow_rect)
+        options_text = font.render("options", True, (255, 255, 255))
+
+    if exit_rect.collidepoint(mouse_pos):
+        shadow_text = font.render("exit", True, (200, 200, 200))
+        shadow_rect = exit_rect.copy()
+        shadow_rect.x -= 2
+        shadow_rect.y += 2
+        screen.blit(shadow_text, shadow_rect)
+        exit_text = font.render("exit", True, (255, 0, 0))
+        
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if button_rect.collidepoint(event.pos):
+            if start_rect.collidepoint(event.pos):
+                print("Start button clicked!")
                 game.main()
-                print("Button clicked!")
+            elif exit_rect.collidepoint(event.pos):
+                print("Exit button clicked!")
+                pygame.quit()
+                sys.exit()
 
-    pygame.draw.rect(screen, (0,0,0), button_rect.inflate(00, 00))
-    screen.blit(button_text, button_rect)
+    # Draw buttons
+    screen.blit(start_text, start_rect)
+    screen.blit(options_text, options_rect)
+    screen.blit(exit_text, exit_rect)
 
     pygame.display.flip()
-
     clock.tick(60)
